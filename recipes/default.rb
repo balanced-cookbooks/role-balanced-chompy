7f9caa0c30707d10d7f1500e7a348a35961e364f
Marshall Jones <marshall@balancedpayments.com>
2013-12-19 14:18:34 -0800

    shuffle position

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,10 @@
+include_recipe 'role-base'
+
 include_recipe 'balanced-monit'
 
 include_recipe 'balanced-rabbitmq::local'
 
 include_recipe 'balanced-postgres::client'
 
-include_recipe 'role-base'
 include_recipe 'balanced-chompy'
 include_recipe 'balanced-chompy::worker'
